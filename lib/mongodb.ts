5170388bc7832a8296ff9a5b02a53b30916cfd26
“Saif <s.aljilani@futeric.com>
2023-12-29 22:07:40 +0300

    update mongoDB file

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,10 +1,15 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const mongoURI = process.env.MONGODB_URI;
+    if (!mongoURI) {
+      throw new Error('MongoDB URI is not defined in the environment variables.');
+    }
+
+    await mongoose.connect(mongoURI);
     console.log("Connected to MongoDB");
   } catch (error) {
-    console.log("Error connecting to MongoDB: ", error);
+    console.log("Error connecting to MongoDB:", error);
   }
 };
